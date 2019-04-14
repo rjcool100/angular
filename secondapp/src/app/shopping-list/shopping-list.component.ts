10f0d345e2512701ad6dc57a864b9a75da19d21d
Rohit Joshi <rojoforlife@Rohits-MacBook-Air.local>
2019-04-14 12:51:36 +0530

    started a second app with more components

---
+++ secondapp/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Rajma', 100),
+    new Ingredient('Chawal', 100)
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
